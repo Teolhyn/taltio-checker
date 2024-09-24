c82fafe34739c7c3affcad2ad9aef23c93500f34
teolhyn <teemu.hynna@gmail.com>
2024-09-24 09:45:45 +0300

    feat: add dashboard

--- utils/utiltools.py
+++ utils/utiltools.py
@@ -38,3 +38,28 @@ def emailify(utuid):
         email_list.append(f"{i}@utu.fi")
 
     return email_list
+
+
+def dashboard():
+    print("\n \n \n \n \n \n \n \n")
+    print(r"""
+          
+ ______   ____  _     ______  ____  ___          __  _        ___   ____  ____     ___  ____  
+|      | /    || |   |      ||    |/   \        /  ]| |      /  _] /    ||    \   /  _]|    \ 
+|      ||  o  || |   |      | |  ||     |      /  / | |     /  [_ |  o  ||  _  | /  [_ |  D  )
+|_|  |_||     || |___|_|  |_| |  ||  O  |     /  /  | |___ |    _]|     ||  |  ||    _]|    / 
+  |  |  |  _  ||     | |  |   |  ||     |    /   \_ |     ||   [_ |  _  ||  |  ||   [_ |    \ 
+  |  |  |  |  ||     | |  |   |  ||     |    \     ||     ||     ||  |  ||  |  ||     ||  .  \
+  |__|  |__|__||_____| |__|  |____|\___/      \____||_____||_____||__|__||__|__||_____||__|\_|
+                                                                                              
+""")
+    print("############################################################################################## \n \n \n")
+
+    commands = (
+        'Available commands: \n'
+        'e - Returns users who are NOT using Taltio \n'
+        'E - Returns emails of users who are NOT using Taltio \n'
+        'u - Returns users who are using Taltio \n'
+        'f - Runs a format check and reports on found formatting errors \n'
+    )
+    print(commands)
